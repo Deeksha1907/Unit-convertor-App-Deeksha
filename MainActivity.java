f3e978465db0f6f3dbc958f8b256f82614463a4a
Deeksha Gupta <113490498+Deeksha1907@users.noreply.github.com>
2022-11-03 00:31:38 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,8 +3,11 @@ package com.unitconverter.app;
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -12,57 +15,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-TextView output;
-EditText Enternum;
-Button milimeter;
-Button centimeter;
-Button kilometer;
-Button miles;
-Button kelvin;
+    Spinner sp1,sp2;
+    EditText ed1;
+    Button bt1;
+    TextView output;
+
     @RequiresApi(api = Build.VERSION_CODES.S)
     @SuppressLint({"WrongViewCast", "MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        output = findViewById(R.id.View);
-        Enternum = findViewById(R.id.editTextNumberDecimal);
-        milimeter = findViewById(R.id.button4);
-        centimeter = findViewById(R.id.button3);
-        kilometer = findViewById(R.id.button2);
-        miles = findViewById(R.id.button);
-        kelvin = findViewById(R.id.button5);
 
-        milimeter.setOnClickListener(view -> {
-            String num = Enternum.getText().toString();
-            float number = Float.parseFloat(num);
-            double milimeter = (number*1000);
-            output.setText("Value in milimeter:\n" + milimeter);
+        sp1 = findViewById(R.id.spfrom);
+        sp2 = findViewById(R.id.spto);
+        ed1 = findViewById(R.id.editTextNumberDecimal);
+        bt1 = findViewById(R.id.button);
+        output = findViewById(R.id.View);
+
+
+        String[] from={"USD","INR",};
+        ArrayAdapter<String> ad = new ArrayAdapter<>(  this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,from);
+        sp1.setAdapter(ad);
+
+        String[] to={"USD","INR","EURO"};
+        ArrayAdapter<String> ad1 = new ArrayAdapter<>( this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,to);
+        sp2.setAdapter(ad1);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double tot = null;
+
+                Double amount = Double.parseDouble(ed1.getText().toString());
+
+                if (sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString() == "INR") {
+                    tot = amount * 82.48;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString() == "EURO") {
+                    tot = amount * 1.01;
+                    output.setText("Value:" + tot);
+                }
+            }
         });
-        centimeter.setOnClickListener(view -> {
-            String num = Enternum.getText().toString();
-            float number = Float.parseFloat(num);
-            double centimeter = (number*100);
-            output.setText("Value in centimeter:\n" + centimeter);
-        });
-        kilometer.setOnClickListener(view -> {
-            String num = Enternum.getText().toString();
-            float number = Float.parseFloat(num);
-            double kilometer = (number/1000);
-            output.setText("Value in kilometer:\n" + kilometer);
-        });
-        miles.setOnClickListener(view -> {
-            String num = Enternum.getText().toString();
-            float number = Float.parseFloat(num);
-            double miles =(number*0.000621);
-            output.setText("Value in miles:\n" + miles);
-        });
-        kelvin.setOnClickListener(view -> {
-            String num = Enternum.getText().toString();
-            float number = Float.parseFloat(num);
-            double kelvin = (number+273.15);
-            output.setText("Value in kelvin:\n" + kelvin);
-        });
+
 
     }
 }
