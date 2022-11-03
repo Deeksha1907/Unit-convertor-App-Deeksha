41bfa1506b350e883aa18791256e202f3d0003fa
Deeksha Gupta <113490498+Deeksha1907@users.noreply.github.com>
2022-11-03 23:22:03 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -34,27 +34,55 @@ public class MainActivity extends AppCompatActivity {
         output = findViewById(R.id.View);
 
 
-        String[] from={"USD","INR",};
+        String[] from={"USD","INR","YEN"};
         ArrayAdapter<String> ad = new ArrayAdapter<>(  this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,from);
         sp1.setAdapter(ad);
 
-        String[] to={"USD","INR","EURO"};
+        String[] to={"USD","INR","YEN"};
         ArrayAdapter<String> ad1 = new ArrayAdapter<>( this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,to);
         sp2.setAdapter(ad1);
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Double tot = null;
+                double tot;
 
-                Double amount = Double.parseDouble(ed1.getText().toString());
+                double amount = Double.parseDouble(ed1.getText().toString());
 
-                if (sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString() == "INR") {
-                    tot = amount * 82.48;
+                if (sp1.getSelectedItem().toString().equals("USD") && sp2.getSelectedItem().toString().equals("INR")) {
+                    tot = amount * 82.78;
                     output.setText("Value:" + tot);
                 }
-                else if (sp1.getSelectedItem().toString() == "USD" && sp2.getSelectedItem().toString() == "EURO") {
-                    tot = amount * 1.01;
+                else if (sp1.getSelectedItem().toString().equals("USD") && sp2.getSelectedItem().toString().equals("YEN")) {
+                    tot = amount * 147.82;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("USD") && sp2.getSelectedItem().toString().equals("USD")) {
+                    tot = amount;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("INR") && sp2.getSelectedItem().toString().equals("YEN")) {
+                    tot = amount * 1.79;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("INR") && sp2.getSelectedItem().toString().equals("USD")) {
+                    tot = amount * 0.012;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("INR") && sp2.getSelectedItem().toString().equals("INR")) {
+                    tot = amount;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("YEN") && sp2.getSelectedItem().toString().equals("USD")) {
+                    tot = amount * 0.0068;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("YEN") && sp2.getSelectedItem().toString().equals("INR")) {
+                    tot = amount * 0.56;
+                    output.setText("Value:" + tot);
+                }
+                else if (sp1.getSelectedItem().toString().equals("YEN") && sp2.getSelectedItem().toString().equals("YEN")) {
+                    tot = amount;
                     output.setText("Value:" + tot);
                 }
             }
